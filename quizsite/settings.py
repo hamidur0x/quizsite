bcf15047cce6bdd477b0288acc26c230ef8a8606
hamidur0x <hamidurrahman0705@gmail.com>
2026-07-18 16:36:29 +0600

    upload

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -126,9 +126,15 @@ import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+import os
+
+DEBUG = False
+
 ALLOWED_HOSTS = [
-    "quizsite-agqu.onrender.com",
     "localhost",
     "127.0.0.1",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://quizsite-agqu.onrender.com",
 ]
