4f7b8439e741c3249dd782e88718217f6957fc4c
hamidur0x <hamidurrahman0705@gmail.com>
2026-07-18 16:20:30 +0600

    upload

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -116,3 +116,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
+ALLOWED_HOSTS = [
+    os.environ.get("RENDER_EXTERNAL_HOSTNAME", ""),
+    "localhost",
+    "127.0.0.1",
+]
