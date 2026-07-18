80e3aa503c9e5c6206997783ef6fdb0272c468f0
hamidur0x <hamidurrahman0705@gmail.com>
2026-07-18 21:36:24 +0600

    Fix setting.py files

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -134,8 +134,9 @@ ALLOWED_HOSTS = [
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("postgresql://quizsite_user:qvadvKvmabyHhfcKHJLeztjcOkQVYuHl@dpg-d9dpogj7uimc73c11ejg-a.oregon-postgres.render.com/quizsite"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
